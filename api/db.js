632d9e1a57c98d52b65c9a8fe9793ba8c009fd08
Jazzie91 <jasminecarter88@gmail.com>
2025-01-04 23:37:28 -0600

    w/p

--- api/db.js
+++ api/db.js
@@ -1,16 +1,23 @@
+const bcrypt = require('bcrypt'); 
+const db = require('./db'); 
+
+const users = []; 
+
 async function getUserByUsername(username) {
   return users.find(user => user.username === username);
 }
 
-
 async function createUser({ username, password }) {
-  const [newUser] = await db('users').insert({ username, password }).returning('*');
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  
+  const [newUser] = await db('users').insert({ username, password: hashedPassword }).returning('*');
   return newUser;
 }
-
 
 function resetUsers() {
   users.length = 0; 
 }
 
-module.exports = { getUserByUsername, createUser, resetUsers, users };
+module.exports = { getUserByUsername, createUser, resetUsers };
